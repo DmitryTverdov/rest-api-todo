7422d067646fa44d91bc1a845850bf87ab9de48d
Dmitry Tverdov <d.tverdov@dunice.net>
2017-08-25 17:17:37 +0300

    Added Procfile and engines

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express')
   , routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var app = express();
 
 // Configuration
 
@@ -32,5 +32,5 @@ app.configure('production', function(){
 app.get('/', routes.index);
 
 app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port 3000");
 });
